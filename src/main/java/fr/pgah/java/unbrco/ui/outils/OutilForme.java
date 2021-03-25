14e55a63f7db483beb60992488a6d725fbf7e47b
Quentin <59879188+quentinDelaporte@users.noreply.github.com>
2021-03-25 16:45:49 +0100

    update read me

--- src/main/java/fr/pgah/java/unbrco/ui/outils/OutilForme.java
+++ src/main/java/fr/pgah/java/unbrco/ui/outils/OutilForme.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
-public class OutilForme extends Outil {
+public abstract class OutilForme extends Outil {
 
     protected Forme forme;
     protected int type;
